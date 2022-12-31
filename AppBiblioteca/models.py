7f468104f4796e9f31444f90dfe5e2eebc3504fd
Eduardo_Gracia <edugracia@gmail.com>
2022-12-31 17:02:22 -0300

    probando avatares

--- AppBiblioteca/models.py
+++ AppBiblioteca/models.py
@@ -7,6 +7,10 @@ class Empleados(models.Model):
     apellido= models.CharField(max_length=50)
     email=models.EmailField()
 
+    class Meta:
+        verbose_name = "Empleado"
+        verbose_name_plural = "Empleados"
+
     def __str__(self):
         return f"{self.nombre} - {self.apellido}"
 
@@ -17,6 +21,10 @@ class Socios(models.Model):
     email= models.EmailField()
     numero_socio= models.IntegerField()
 
+    class Meta:
+        verbose_name = "Socio"
+        verbose_name_plural = "Socios"
+
     def __str__(self):
         return f"{self.nombre} - {self.apellido} - {str(self.numero_socio)}"
 
@@ -25,6 +33,10 @@ class Libros(models.Model):
     titulo= models.CharField(max_length=50)
     autor= models.CharField(max_length=50)
     codigo= models.IntegerField()
+
+    class Meta:
+        verbose_name = "Libro"
+        verbose_name_plural = "Libros"
 
     def __str__(self):
         return f"{self.titulo} - {self.autor} - {str(self.codigo)}"
